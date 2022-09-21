06d7978b3a5698641592d5a8c9745458d18edb4f
mashruf <mashrufrohon@gmail.com>
2022-09-22 02:59:08 +0600

    showing comment to the html complete

--- js/jsonplaceholder.js
+++ js/jsonplaceholder.js
@@ -4,11 +4,24 @@ const loadComment = () => {
         .then(data => displayComment(data));
 }
 
-loadComment();
+// loadComment();
 
 const displayComment = (comments) => {
     comments.forEach(comment => {
-        console.log(comment);
+        const showComment = document.getElementById('show-comment');
+        const div = document.createElement('div');
+        div.classList.add('comment');
+
+        div.innerHTML = `
+            <h3>${comment.id}</h3>
+            <p>${comment.body}</p>
+        `;
+        const comments = document.getElementsByClassName('comment');
+        for (const comment of comments) {
+            comment.style.border = '1px solid red';
+            comment.style.marginBottom = '5px';
+        }
+        showComment.appendChild(div);
     });
 
 }
